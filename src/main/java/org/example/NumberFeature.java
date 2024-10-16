2536c124225f03b1b6b780a49e62ee4bf388693f
courseSBS <courseSBS@github.com>
2024-10-16 15:04:01 +0800

    complete

--- src/main/java/org/example/NumberFeature.java
+++ src/main/java/org/example/NumberFeature.java
@@ -9,6 +9,21 @@ public class NumberFeature {
         Scanner in = new Scanner(System.in);
         inputNumber = in.nextInt();
         // 你的代码逻辑
+        int count=0;//二进制此时的位数，当前为0
+        int count1 = 1;//数字当前的位数
+        int n;//余数
+
+        do{
+            n = inputNumber % 10;
+            if (n % 2 == count1 % 2)
+            {
+                result = result+(int)(1 * Math.pow(2,count));
+
+            }
+            count += 1;//无论如何都会自增，一定不要写到if循环里面
+            count1 += 1;//切记不要写到if循环里面
+            inputNumber = inputNumber / 10;
+        }while(inputNumber != 0);
 
         System.out.println(result);
     }
